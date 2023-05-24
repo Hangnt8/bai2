fb182fa185793ba890559b9439607bab99ec0c66
Hangnt8 <133937805+Hangnt8@users.noreply.github.com>
2023-05-24 13:25:27 +0700

    Update cau11.php

--- cau11.php
+++ cau11.php
@@ -1,15 +1,6 @@
 <?php
 /* 11.Viết một chương trình PHP để loại bỏ ký tự đầu tiên của một chuỗi sử dụng hàm ltrim().
 */
-/* function removeFirstCharacter($string) {
-    $result = substr($string, 1);
-    return $result;
-}
-
-    $input = "Hello!";
-    $removedCharacter = removeFirstCharacter($input);
-    echo "Kết quả: " . $removedCharacter;
-*/
 function removeFirstCharacter($string) {
     $firstCharacter = $string[0]; // Lấy ký tự đầu tiên của chuỗi
     $result = ltrim($string, $firstCharacter); // Loại bỏ ký tự đầu tiên
